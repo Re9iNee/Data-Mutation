724f67885e5659187932c3669c1b7282124ccef4
Re9iNee <attarzadeh76@gmail.com>
2024-08-26 19:29:16 +0330

    fixed some styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
     <main className='p-5 flex flex-col gap-5'>
       <h1 className='font-bold'>Data Mutation example</h1>
 
-      <section aria-label='user-list' className='border rounded-sm max-w-[50%]'>
+      <section aria-label='user-list' className='border ruonded-sm'>
         <UserTable users={users} />
       </section>
     </main>
