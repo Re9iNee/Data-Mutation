102c1060b9e934d3fd45704e6f36b373d2aaed52
Re9iNee <attarzadeh76@gmail.com>
2024-08-26 19:13:59 +0330

    adding utility class

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export type TransformToFieldErrorsType<T> = {
+  [K in keyof T]?: T[K][] | undefined;
+};
