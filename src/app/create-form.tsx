603feda5d35dfb50ea1c82240107aef20e98859e
Re9iNee <attarzadeh76@gmail.com>
2024-08-29 10:40:34 +0330

    acessibility

--- src/app/create-form.tsx
+++ src/app/create-form.tsx
@@ -28,12 +28,15 @@ export default function UserCreateForm() {
                 name='name'
                 type='text'
                 placeholder='Enter name'
+                aria-describedby='name-error'
                 className='peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500'
               />
               <UserCircleIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
             </div>
           </div>
-          {state?.name && <p className='text-red-500'>{state.name}</p>}
+          <div id='name-error' aria-live='polite' aria-atomic='true'>
+            {state?.name && <p className='text-red-500'>{state.name}</p>}
+          </div>
         </div>
         {/* email input */}
         <div className='mb-4'>
